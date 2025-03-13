9eb56fbd84d82857731a96ca9e7021c3a2cbbcab
Noah Mclaughlin <L00182257@ATU.ie>
2025-03-13 16:46:23 +0000

    Update dentistDB.sql

--- dentistDB.sql
+++ dentistDB.sql
@@ -44,15 +44,14 @@ CREATE TABLE Appointment (
     FOREIGN KEY (DentistID) REFERENCES Dentist(DentistID) ON DELETE CASCADE
 );
 
--- Inserting Patients
 INSERT INTO Patient (LastName, FirstName, DateOfBirth, Email, Street, Town, County, Eircode, MedicalCard)
 VALUES
     ('Doe', 'John', '1985-06-15', 'johndoe@example.com', '123 Main St', 'Dublin', 'Dublin', 'D01 ABC', TRUE),
     ('Smith', 'Emily', '1990-11-30', 'emilysmith@example.com', '456 Oak St', 'Cork', 'Cork', 'T12 XYZ', FALSE),
     ('Brown', 'Michael', '1975-02-20', 'michaelbrown@example.com', '789 Pine Rd', 'Limerick', 'Limerick', 'V94 123', TRUE),
-    ('McLaughlin', 'Noah', '10/10/2005', 'BigMan123@gmail.com', '16 Ohiovile', 'Bunc', 'Donegal', 'F32 JH33', TRUE),
-    ('Friel', 'Darren', '20/04/1899', 'HungDaddy8@Hotmail.com', '1 SwagSt', 'Novigrad', 'Redania', 'F69 HH88', FALSE),
-    ('Olana', 'Rox', '14/07/2013', 'DealCollege4Life@outlook.com', '18 WomanSt', 'Killybegs', 'Odessa', 'F12 RT21', FALSE);
+    ('McLaughlin', 'Noah', '2005-10-10', 'BigMan123@gmail.com', '16 Ohiovile', 'Bunc', 'Donegal', 'F32 JH33', TRUE),
+    ('Friel', 'Darren', '1899-04-20', 'HungDaddy8@Hotmail.com', '1 SwagSt', 'Novigrad', 'Redania', 'F69 HH88', FALSE),
+    ('Olana', 'Rox', '2013-07-14', 'DealCollege4Life@outlook.com', '18 WomanSt', 'Killybegs', 'Odessa', 'F12 RT21', FALSE);
 
 -- Inserting Dentists
 INSERT INTO Dentist (LastName, FirstName, AwardingBody, Specialty, DateOfBirth, PhoneNo)
@@ -60,7 +59,7 @@ VALUES
     ('Taylor', 'Sarah', 'Irish Dental Council', 'Orthodontics', '1982-03-10', '353 1 2345678'),
     ('Johnson', 'Mark', 'Irish Dental Council', 'General Dentistry', '1978-08-22', '353 1 8765432'),
     ('Williams', 'Olivia', 'Royal College of Surgeons in Ireland', 'Periodontics', '1990-07-14', '353 1 1122334'),
-    ('Kako', 'Ibrahim', 'GoatSelecters', 'CodeMaster', '11/09/2090', '087 1 2345543');
+    ('Kako', 'Ibrahim', 'GoatSelecters', 'CodeMaster', '2090-11-11', '087 1 2345543');
 
 
 -- Inserting Treatments
