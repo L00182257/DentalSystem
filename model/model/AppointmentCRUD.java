67a89437f27ce275d3d36866efd367f6fbd41d0b
Roksolana Kryvulia <151508080+roxaaaaa@users.noreply.github.com>
2025-03-14 11:05:31 +0000

    Update AppointmentCRUD.java

--- model/model/AppointmentCRUD.java
+++ model/model/AppointmentCRUD.java
@@ -19,7 +19,7 @@ public class AppointmentCRUD {
     private static Connection getConnection() throws SQLException {
         String url = "jdbc:mysql://localhost:3306/dentistdb"; // Change database name
         String username = "root"; // username
-        String password = "TKJjoq2d."; // password
+        String password = "password"; // password
         return DriverManager.getConnection(url, username, password);
     }
 
