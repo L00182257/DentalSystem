a6676223a1c06f2cef2d8e62acf3725893f6dd87
Ibrahim Kako <l00182089@atu.ie>
2025-04-10 00:04:37 +0100

    Update dentistDB.sql

--- dentistDB.sql
+++ dentistDB.sql
@@ -58,28 +58,16 @@ VALUES
     ('Williams', 'Emma', '1978-03-12', '+3530897891188' ,'emma.williams@example.com', '321 Pine St', 'Limerick', 'Limerick', 'V94 JKL', TRUE, 0.00),
     ('Brown', 'David', '1995-07-30', '+3530897894567' ,'david.brown@example.com', '654 Cedar St', 'Waterford', 'Waterford', 'X91 MNO', FALSE, 0.00),
     ('Jones', 'Sophia', '1988-09-25', '+3530897890033' ,'sophia.jones@example.com', '987 Birch St', 'Kilkenny', 'Kilkenny', 'R95 PQR', TRUE, 0.00),
-    ('Garcia', 'James', '1980-12-15', '+353089832488' ,'james.garcia@example.com', '135 Maple St', 'Wexford', 'Wexford', 'Y35 STU', FALSE, 0.00),
-    ('Martinez', 'Olivia', '1992-05-10', '+3530697890088' ,'olivia.martinez@example.com', '246 Spruce St', 'Sligo', 'Sligo', 'F91 VWX', TRUE, 0.00),
-    ('Davis', 'Liam', '1984-08-22', '+3530897810088' ,'liam.davis@example.com', '357 Fir St', 'Donegal', 'Donegal', 'F94 YZA', FALSE, 0.00),
-    ('Rodriguez', 'Isabella', '1991-01-18','+3530897430088' , 'isabella.rodriguez@example.com', '468 Willow St', 'Kerry', 'Kerry', 'V93 BCD', TRUE, 0.00),
-    ('Martinez', 'Noah', '1986-04-28','+3530845890088' , 'noah.martinez@example.com', '579 Ash St', 'Tipperary', 'Tipperary', 'E34 EFG', FALSE, 0.00);
+    ('Garcia', 'James', '1980-12-15', '+353089832488' ,'james.garcia@example.com', '135 Maple St', 'Wexford', 'Wexford', 'Y35 STU', FALSE, 0.00);
 
 -- Inserting Dentists
 INSERT INTO Dentist (LastName, FirstName, AwardingBody, Specialty, DateOfBirth, PhoneNo)
 VALUES
-    ('Taylor', 'Sarah', 'Irish Dental Council', 'Orthodontics', '1982-03-10', '+3530897897788'),
-    ('Johnson', 'Mark', 'Irish Dental Council', 'General Dentistry', '1978-08-22', '+3530897897732'),
-    ('Williams', 'Olivia', 'Royal College of Surgeons in Ireland', 'Periodontics', '1990-07-14', '+3530877567788'),
-    ('Smith', 'John', 'American Dental Association', 'Pediatric Dentistry', '1975-06-15', '+3530876543210'),
-    ('Johnson', 'Emily', 'British Dental Association', 'Periodontics', '1988-11-22', '+3530865432109'),
-    ('Williams', 'Michael', 'Canadian Dental Association', 'Oral Surgery', '1980-01-30', '+3530854321098'),
-    ('Brown', 'Jessica', 'Australian Dental Association', 'Cosmetic Dentistry', '1990-04-05', '+3530843210987'),
-    ('Jones', 'David', 'New Zealand Dental Association', 'Endodontics', '1978-09-12', '+3530832109876'),
-    ('Garcia', 'Laura', 'Mexican Dental Association', 'Prosthodontics', '1985-12-25', '+3530821098765'),
-    ('Martinez', 'Daniel', 'South African Dental Association', 'General Dentistry', '1992-07-19', '+3530810987654'),
-    ('Davis', 'Sophia', 'Indian Dental Association', 'Oral Medicine', '1983-10-28', '+3530809876543'),
-    ('Rodriguez', 'James', 'European Dental Association', 'Public Health Dentistry', '1979-02-14', '+3530798765432'),
-    ('Martinez', 'Olivia', 'World Dental Federation', 'Dental Anesthesiology', '1986-05-03', '+3530787654321');
+    ('Taylor', 'Sarah', 'Irish Dental Council', 'All', '1982-03-10', '+3530897897788'),
+    ('Johnson', 'Mark', 'Irish Dental Council', 'All', '1978-08-22', '+3530897897732'),
+    ('Williams', 'Olivia', 'Royal College of Surgeons in Ireland', 'All', '1990-07-14', '+3530877567788'),
+    ('Smith', 'John', 'American Dental Association', 'All', '1975-06-15', '+3530876543210'),
+    ('Martinez', 'Olivia', 'World Dental Federation', 'All', '1986-05-03', '+3530787654321');
 
 
 -- Inserting Treatments
@@ -95,6 +83,17 @@ VALUES
     ('Dental Cleaning',30,30),
     ('Tooth Extractions',30,30),
     ('Cavity Filling', 200.00, 30);
+    
+    -- Inserting Appointments
+INSERT INTO Appointment (DateOfAppointment, TimeOfAppointment, Attended, TreatmentID, PatientID, DentistID)
+VALUES
+    ('2025-04-11', '10:00:00', TRUE, 2, 1, 1),  -- John Doe with Sarah Taylor for Dental Checkup
+    ('2025-04-11', '10:30:00', FALSE, 5, 4, 3), -- Alice Smith with Olivia Williams for Dental Cleaning
+    ('2025-04-11', '11:00:00', TRUE, 3, 2, 2),  -- Emily Smith with Mark Johnson for Root Canal
+    ('2025-04-12', '9:30:00', FALSE, 1, 5, 4), -- Michael Johnson with John Smith for Whitening
+    ('2025-04-12', '11:00:00', TRUE, 10, 6, 5); -- Emma Williams with Olivia Martinez for Cavity Filling
+    
+    select * from appointment;
     
 ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
 FLUSH PRIVILEGES;
